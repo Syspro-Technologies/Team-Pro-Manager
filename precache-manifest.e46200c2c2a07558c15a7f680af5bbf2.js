4892fe091c81415bafecbb3ef66706d64efe1bf0
unknown <kulkarni.aakash@gmail.com>
2020-11-21 18:29:53 +0530

    Updates

---
+++ precache-manifest.e46200c2c2a07558c15a7f680af5bbf2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fee09d2a3971576e8ee0865ee1cb2c60",
+    "url": "/Team-Pro-Manager/index.html"
+  },
+  {
+    "revision": "134796c5a335a13af698",
+    "url": "/Team-Pro-Manager/static/css/2.4819d237.chunk.css"
+  },
+  {
+    "revision": "df2b678fc62277408edb",
+    "url": "/Team-Pro-Manager/static/css/main.25aa6262.chunk.css"
+  },
+  {
+    "revision": "134796c5a335a13af698",
+    "url": "/Team-Pro-Manager/static/js/2.c8f8abe8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Team-Pro-Manager/static/js/2.c8f8abe8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df2b678fc62277408edb",
+    "url": "/Team-Pro-Manager/static/js/main.cd8fc735.chunk.js"
+  },
+  {
+    "revision": "ec4c30df9b5a66a2adc5",
+    "url": "/Team-Pro-Manager/static/js/runtime-main.99cc14ac.js"
+  }
+]);
